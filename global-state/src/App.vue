8b3ebe5e94c46f5103a180a3c5a76f24d1341baf
Rodrigo Boniatti <boniatti.rodrigo@gmail.com>
2019-11-26 22:14:27 -0300

    Global state smell

---
+++ global-state/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <UsersList />
+  </div>
+</template>
+
+<script>
+import UsersList from './components/UsersList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    UsersList
+  }
+}
+</script>
---
+++ global-state/src/components/UsersList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <ul v-for="user in $store.state.users" v-bind:key="user" data-test="users-list">
+      <li>{{ user }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersList',
+  mounted() {
+    this.$store.dispatch('fetchUsers')
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+</style>
